fc48f054f70e6511ff9d53cd61b6aff046a3efeb
Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>
2017-05-02 15:36:03 -0300

    FDTMCToParamTest: Applying Extract Class tecnique of Moving Characteristics Between Objects in ParamModel Class
    Code smells:
    ParamModel class with too much responsibility
    
    Refactoring operation:
    Extract Class
    
    Class, attribute or method impacted:
    Class: ParamModel, Command; Method: generateCommand, getCommands
    
    Refactoring Result:
    Class Command moved to a new file with the methods generateCommand and getCommands, wich now belongs to it
    
    Signed-off-by: Izabela Cardoso <cizabelacristina@gmail.com>
    Signed-off-by: Igor Ribeiro Barbosa Duarte <igor.ribeiro.duarte@gmail.com>

--- src/paramwrapper/ParamModel.java
+++ src/paramwrapper/ParamModel.java
@@ -38,7 +38,9 @@ class ParamModel {
 			stateVariable = fdtmc.getVariableName();
 		}
 		initialState = fdtmc.getInitialState().getIndex();
-		commands = getCommands(fdtmc);
+		
+		commands = new Command(initialState).getCommands(fdtmc);
+		
 		labels = getLabels(fdtmc);
 		stateRangeStart = Collections.min(commands.keySet());
 		// PARAM não deixa declarar um intervalo com apenas um número.
@@ -72,34 +74,6 @@ class ParamModel {
 			}
 		}
 		return labeledStates;
-	}
-
-	private Command generateCommand(Entry<State, List<Transition>> entry, int initState) {
-		Command command = new Command(initState);
-		if (entry.getValue() != null) {
-		    for (Transition transition : entry.getValue()) {
-		        command.addUpdate(transition.getProbability(),
-		                          transition.getTarget().getIndex());
-		    }
-		} else {
-		    // Workaround: manually adding self-loops in case no
-		    // transition was specified for a given state.
-		    command.addUpdate("1", initState);
-		}
-		
-		return command;
-	}
-	
-	private Map<Integer, Command> getCommands(FDTMC fdtmc) {
-		Map<Integer, Command> tmpCommands = new TreeMap<Integer, Command>();
-		for (Entry<State, List<Transition>> entry : fdtmc.getTransitions().entrySet()) {
-		    int initState = entry.getKey().getIndex();
-		    
-		    Command command = generateCommand(entry, initState);
-		    
-			tmpCommands.put(initState, command);
-		}
-		return tmpCommands;
 	}
 
 	private Set<String> getParameters(Collection<Command> commands) {
@@ -160,40 +134,5 @@ class ParamModel {
 		module += labelsToModule();
 		
 		return module;
-	}
-}
-
-class Command {
-	private int initialState;
-	private List<String> updatesProbabilities;
-	private List<Integer> updatesActions;
-
-	public Command(int initialState) {
-		this.initialState = initialState;
-        this.updatesProbabilities = new LinkedList<String>();
-        this.updatesActions = new LinkedList<Integer>();
-	}
-
-	public void addUpdate(String probability, int update) {
-		updatesProbabilities.add(probability);
-		updatesActions.add(update);
-	}
-
-	public Collection<String> getUpdatesProbabilities() {
-		return updatesProbabilities;
-	}
-
-	public String makeString(String stateVariable) {
-		String command = "[] "+stateVariable+"="+initialState+" -> ";
-		boolean needsPlus = false;
-		for (int i = 0; i < updatesProbabilities.size(); i++) {
-		    if (needsPlus) {
-		        command += " + ";
-		    } else {
-		        needsPlus = true;
-		    }
-			command += "("+updatesProbabilities.get(i)+") : ("+stateVariable+"'="+updatesActions.get(i)+")";
-		}
-		return command+";";
 	}
 }
